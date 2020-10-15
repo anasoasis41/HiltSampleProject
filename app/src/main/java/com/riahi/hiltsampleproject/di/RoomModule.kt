f71c9b7a7ccc9145e7ed7867de223ec442c5bc02
Anas RIAHI <anas.riahi@smile-maroc.com>
2020-10-15 16:56:54 +0100

    Dependency Injection - Room

---
+++ app/src/main/java/com/riahi/hiltsampleproject/di/RoomModule.kt
@@ -0,0 +1,37 @@
+package com.riahi.hiltsampleproject.di
+
+import android.content.Context
+import androidx.room.Room
+import com.riahi.hiltsampleproject.room.BlogDao
+import com.riahi.hiltsampleproject.room.BlogDatabase
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.android.components.ApplicationComponent
+import dagger.hilt.android.qualifiers.ApplicationContext
+import javax.inject.Singleton
+
+
+@InstallIn(ApplicationComponent::class)
+@Module
+object RoomModule {
+
+    @Singleton
+    @Provides
+    fun provideBlogDb(@ApplicationContext context: Context): BlogDatabase {
+        return Room
+            .databaseBuilder(
+                context,
+                BlogDatabase::class.java,
+                BlogDatabase.DATABASE_NAME
+            )
+            .fallbackToDestructiveMigration()
+            .build()
+    }
+
+    @Singleton
+    @Provides
+    fun provideBlogDAO(blogDatabase: BlogDatabase): BlogDao {
+        return blogDatabase.blogDao()
+    }
+}
